d2d216a77dad2a30870811087f8947f4c4b35553
MatheusGoes23 <matheusgoes23@gmail.com>
2021-06-25 15:51:19 -0300

    Developed hypermedia

--- src/main/java/com/matheusgoes23/productapi/model/Product.java
+++ src/main/java/com/matheusgoes23/productapi/model/Product.java
@@ -2,15 +2,17 @@ package com.matheusgoes23.productapi.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
 @Data
 @Table(name = "tb_product")
-public class Product {
+public class Product extends RepresentationModel<Product> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
